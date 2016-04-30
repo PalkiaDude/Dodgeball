32f425f2135b3f27914917a4a073e88e64b9be29
PalkiaDude <teammegamanz@gmail.com>
2016-04-30 13:18:16 +0800

    Update Main.php

--- src/Dodgeball/Main.php
+++ src/Dodgeball/Main.php
@@ -15,27 +15,16 @@
 $this->getServer()->getPluginManager()->registerEvents($this,$this);
         $this->getLogger()->info(TextFormat::GREEN . "Dodgeball Plugin has been enabled!");
     }
-   //setup game system via sign
+   
    public function onHurt(ProjectileHitEvent $event) {
-     $p = $event->getEntity();
-  if($event instanceof Snowball /* i picked arrow cause it was the first to come to mind*/){
+     $p = $event->getPlayer();
+     $level = $p->getLevel->getName();
+     $arena = getLevelByName("DodgeballArena");
+  if($event instanceof Snowball){
+  if($level === $arena){
   $this->getServer()->broadcastMessage("Someone has been killed");
   $p->setHealth(0);
   }
    }
-  public function gameFunction(){
-    //i will use a plugintask and call this function via: $this->getOwner()->gameFunction();
-    if($this->Status===0){
-      //do nothing ofc
-    }
-  if($this->Status===1)
-  foreach($this->Players as $p){
-  if(isset($this->Player[$p->getName()])){
-    
-    $p->setLevel($this->config->get("gameWorld"));
-    
-  }
-  }
-  
-  }
-   }
+}
+  public function 
